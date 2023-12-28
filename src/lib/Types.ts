c78a082ccd15a0aef9a78a30f6dec2771751fda8
madmti <supermatrix9@gmail.com>
2023-12-27 22:38:08 -0300

    Added friends field to UserData model

--- src/lib/Types.ts
+++ src/lib/Types.ts
@@ -1,50 +1,52 @@
-import { type ObjectId } from "mongoose";
+import { type ObjectId } from 'mongoose';
 
 export type UserLock = {
-    _id: ObjectId,
-    name: String,
-    email: String,
-    password: String,
-    isSuper: boolean
+	_id: ObjectId;
+	name: String;
+	email: String;
+	password: String;
+	isSuper: boolean;
+	ref: ObjectId | UserData;
 };
 
 export type Message = {
-    _id: ObjectId,
-    from: ObjectId,
-    author: Author,
-    content: String,
-    date: Date
-}
+	_id: ObjectId;
+	from: ObjectId;
+	author: Author;
+	content: String;
+	date: Date;
+};
 
 export type Author = {
-    _id: ObjectId,
-    alias: String | null,
-    name: String
-}
+	_id: ObjectId;
+	alias: String | null;
+	name: String;
+};
 
 export type Channel = {
-    _id: ObjectId,
-    from: ObjectId,
-    name: String,
-    is: 'text' | 'voice' | 'video'
-    history: Message[] | null
-}
+	_id: ObjectId;
+	from: ObjectId;
+	name: String;
+	is: 'text' | 'voice' | 'video';
+	history: Message[] | null;
+};
 
 export type Server = {
-    _id: ObjectId,
-    isPublic: boolean,
-    key: string | null,
-    name: String,
-    channels: Channel[] | null,
-    description: string | null,
-    super: UserData | ObjectId | null
-}
+	_id: ObjectId;
+	isPublic: boolean;
+	key: string | null;
+	name: String;
+	channels: Channel[] | null;
+	description: string | null;
+	super: UserData | ObjectId | null;
+};
 
 export type UserData = {
-    _id: ObjectId,
-    alias: String | null,
-    name: String | null,
-    imgUrl: String | null,
-    servers: Server[] | null,
-    created: Number
-}
+	_id: ObjectId;
+	alias: String | null;
+	name: String | null;
+	imgUrl: String | null;
+	servers: Server[] | null;
+	created: Number;
+	friends: String[] | UserData[] | null;
+};
--- src/models/userdata.ts
+++ src/models/userdata.ts
@@ -1,13 +1,13 @@
-import { Schema, model } from "mongoose";
-import { type UserData } from "@/lib/Types";
-
+import { Schema, model } from 'mongoose';
+import { type UserData } from '../lib/Types';
 
 const UserDataSchema = new Schema<UserData>({
-    alias: { type: String, required:false },
-    imgUrl: { type: String, required:false },
-    name: { type: String, required: true, ref:'AuthLogin' },
-    servers: [{ type: Schema.Types.ObjectId, ref:'Servers' }],
-    created: { type:Number, required:true, default:0 }
+	alias: { type: String, required: false },
+	imgUrl: { type: String, required: false },
+	name: { type: String, required: true, ref: 'AuthLogin' },
+	servers: [{ type: Schema.Types.ObjectId, ref: 'Servers' }],
+	created: { type: Number, required: true, default: 0 },
+	friends: [{ type: Schema.Types.ObjectId, ref: 'UserData' }],
 });
 
 export const UserDataModel = model<UserData>('UserData', UserDataSchema);
